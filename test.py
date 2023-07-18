02e0d0c3c52b78a52f24cd038966a8e1e136466d
Charles <1140601003@qq.com>
2023-07-18 13:42:42 +0800

    demo

--- test.py
+++ test.py
@@ -8,9 +8,13 @@ from tenacity import retry, stop_after_attempt, stop_after_delay, wait_chain, wa
 
 
 
-@retry(stop=stop_after_attempt(10), wait=wait_fixed(3))
-async def xiaodigu_hot():
-    print(":")
-    result = 5 / 0
+# @retry(stop=stop_after_attempt(10), wait=wait_fixed(3))
+# async def xiaodigu_hot():
+#     print(":")
+# #     result = 5 / 0
     
-asyncio.run(xiaodigu_hot())
+# asyncio.run(xiaodigu_hot())
+
+
+dd =[1,2,3,4,5,6,7,8,9,10,]
+print(dd)
