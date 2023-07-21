293dc2ce72ea7b68c1584a753b9c4a24ff9b2973
Charles <1140601003@qq.com>
2023-07-21 17:07:24 +0800

    debug

--- test.py
+++ test.py
@@ -1,7 +1,23 @@
+import re
 
-import pyhttpx
- 
-sess = pyhttpx.HttpSession()
-r = sess.get('https://httpbin.org/get', headers={'User-Agent': '3301'}, cookies={'k': '3301'})
-print(r.status_code)
-print(r.request.raw)
+def check_word_in_text(text):
+    word_list = ['?']  # 添加其他关键词到这里
+    escaped_words = [re.escape(word) for word in word_list]
+    pattern = '|'.join(escaped_words)
+
+    # 使用负向断言排除链接中的?
+    pattern = f"(?!https?://)(?:{pattern})"
+    
+    regex = re.compile(pattern)
+    match = regex.search(text)
+    if match:
+        print(f"匹配到关键词: {match.group()}")
+        return False
+    else:
+        return True
+
+# 测试
+text1 = "这是一个测试文本，包含一个关键词"
+text2 = "这是另一个测试文本，没有关键词https://example.com/test?param=1"
+print(check_word_in_text(text1))
+print(check_word_in_text(text2))
