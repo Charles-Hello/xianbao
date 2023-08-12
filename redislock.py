93d17ceb246197ab608883d0e5f3b29a02bb0cdd
Charles <1140601003@qq.com>
2023-08-12 13:34:47 +0800

    🚑

--- redislock.py
+++ redislock.py
@@ -2,17 +2,17 @@ import redis
 from config import redishost,redisport
 pool = redis.ConnectionPool(host=redishost, port=redisport, decode_responses=True)
 r = redis.Redis(connection_pool=pool)
-
+import time 
 
 # 创建一个锁
-keyname = 'mylock1'
+keyname = 'mylock4'
 lock = r.lock(keyname) #创建一个锁
 r.expire(keyname, 20) #设置锁的过期时间为30秒
 # try:
 #     # 获取锁
 #     lockres = lock.acquire()
 #     print(lockres)
-#     time.sleep(1)
+#     time.sleep(7)
 # except Exception as e:
 #     print(e)
 # finally:
