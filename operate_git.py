5af7eeb2e083adc019e3eccee348ec72aa64eec9
Charles <1140601003@qq.com>
2023-09-17 08:46:01 +0800

    update word_list

--- operate_git.py
+++ operate_git.py
@@ -13,6 +13,10 @@ repo.git.add('--all')
 # 提交更改
 repo.git.commit(m='update word_list')
 
+
+# 创建并切换到master分支
+repo.git.checkout('-b', 'master')
+
 # 推送到GitHub
 origin = repo.remote(name='origin')
 origin.push('master:mybranch')
