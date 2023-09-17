c27aaf0d55f3714ea87591403e557627318ddf16
Charles <1140601003@qq.com>
2023-09-17 10:26:30 +0800

    update word_list

--- operate_git.py
+++ operate_git.py
@@ -6,12 +6,17 @@ def git_push():
         # 初始化git仓库
         repo = git.Repo(path)
 
-        # # 切换到master分支
-        # repo.git.checkout('master')
+        # 添加所有更改到暂存区
+        repo.git.add('--all')
+        
+        # 提交更改
+        repo.git.commit(m='update word_list')
+        # 切换到master分支
+        repo.git.checkout('origin')
 
         # 拉取远程分支（master:mybranch）
         origin = repo.remote(name='origin')
-        origin.pull('mybranch')
+        origin.pull('origin:mybranch')
 
         # 添加所有更改到暂存区
         repo.git.add('--all')
@@ -20,7 +25,7 @@ def git_push():
         repo.git.commit(m='update word_list')
 
         # 推送到GitHub
-        origin.push('mybranch')
+        origin.push('origin:mybranch')
         return True, True
     except Exception as e:
         print(e)
