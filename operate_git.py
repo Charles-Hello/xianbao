29754fe6473a1901da9d4f55eefb43fb24e3bee7
Charles <1140601003@qq.com>
2023-09-17 08:51:12 +0800

    update word_list

--- operate_git.py
+++ operate_git.py
@@ -1,23 +1,23 @@
 import git
 
-
-
 path = r"C:\\Users\\a1140\\Desktop\\pybot\\xianbao"
-# 初始化git仓库
-repo = git.Repo(path)
+def git_push():
+
+    # 初始化git仓库
+    repo = git.Repo(path)
+
+    # 添加所有更改到暂存区
+    repo.git.add('--all')
+
+    # 提交更改
+    repo.git.commit(m='update word_list')
 
 
-# 添加所有更改到暂存区
-repo.git.add('--all')
+    # 切换到master分支
+    repo.git.checkout('master')
 
-# 提交更改
-repo.git.commit(m='update word_list')
+    # 推送到GitHub
+    origin = repo.remote(name='origin')
+    origin.push('master:mybranch')
 
-
-# 切换到master分支
-repo.git.checkout('master')
-
-# 推送到GitHub
-origin = repo.remote(name='origin')
-origin.push('master:mybranch')
-
+git_push()
